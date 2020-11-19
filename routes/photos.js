c5b45bca316c0cdcf6d1ec416ed9e398f3221a9d
Parthiv Mohan <parthiv.mohan@gmail.com>
2020-11-18 21:36:44 -0800

    update for production only

--- routes/photos.js
+++ routes/photos.js
@@ -4,7 +4,6 @@ var photo_controller = require('../controllers/photo_controller');
 
 var router = express.Router();
 
-router.options('/create', cors());
 router.post('/create', photo_controller.photo_create);
 
 router.get('/location/:locationId', photo_controller.photo_get);
