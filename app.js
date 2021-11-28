e1c0c6a85daa1c23c89a49782e18b31097db44bb
Parthiv Mohan <parthiv.mohan@gmail.com>
2021-11-28 17:11:16 -0500

    cors

--- app.js
+++ app.js
@@ -53,8 +53,11 @@ var app = express();
 //     },
 //     credentials: true
 // }
-app.use(cors({credentials: true, origin: true}));
-app.options('*', cors({credentials: true, origin: true}));
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
