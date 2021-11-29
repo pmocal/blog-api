49a0c588ad9393e12efd8506e685ce0f6f5eef03
Parthiv Mohan <parthiv.mohan@gmail.com>
2021-11-28 22:31:11 -0500

    cors

--- app.js
+++ app.js
@@ -53,9 +53,9 @@ var app = express();
 //     },
 //     credentials: true
 // }
-app.all('/*', function(req, res, next) {
+app.use('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
