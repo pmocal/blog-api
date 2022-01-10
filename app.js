15598059fe2a48f6c15791c9d18eeaded4adb11c
Parthiv Mohan <parthiv.mohan@gmail.com>
2022-01-09 20:47:26 -0500

    a

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(passport.initialize());
 
-app.use(cors());
+app.use(cors({ origin: origin, credentials: true }));
 
 app.use('/', indexRouter);
 app.use('/auth', auth);
